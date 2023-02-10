3de3d2eff48490b8ead478fe2c8f8a235be1b284
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-02-10 09:16:27 +0530

    Day05
    Method Overloading, Overriding, Multi-level Inheritance.

---
+++ src/day05/test01/Program01.java
@@ -0,0 +1,24 @@
+package day05.test01;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(20,30);
+		add(1.5, 2.5);
+		add(20,30,10);
+	}
+	public static void add(int a, int b) 
+	{
+		System.out.println(a+b);
+	}
+	public static void add(double a, double b) 
+	{
+		System.out.println(a+b);
+	}
+	public static void add(int a, int b, int c) 
+	{
+		System.out.println(a+b+c);
+	}
+
+}
---
+++ src/day05/test02/Program03.java
@@ -0,0 +1,14 @@
+package day05.test02;
+
+public class Program03 extends Program02{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		display1();
+		display2();
+		Program03 x=new Program03();
+		x.display1NS();
+		x.display2NS();
+	}
+
+}
