567265cec6b2b77bcc8b4622b883092f01831d31
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-02-13 08:41:29 +0530

    Day 07

---
+++ src/day07/Program01.java
@@ -0,0 +1,23 @@
+package day07;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println("Start Program!!!");
+		if (a>b) //if added ;, it will be considered as a stmt and doesn't checks for condition
+		{
+			System.out.println("Good Day!!!");
+			System.out.println("Good Morning!!!");
+		}
+		if (a<b)
+		
+			System.out.println("Good Day!!!"); //only one stmt, no braces req
+		
+	
+		System.out.println("End Program!!!");
+	}
+
+}
---
+++ src/day07/Program02.java
@@ -0,0 +1,23 @@
+package day07;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+				System.out.println("Start Program!!!");
+		if (a>b) //if added ;, it will be considered as a stmt and doesn't checks for condition
+		{
+			System.out.println("a is greater");
+		}
+
+		else
+		{
+			System.out.println("b is greater"); //only one stmt, no braces req
+		}
+		
+	}
+
+}
