8ae0fc21f5487d9c1fb4154ce19c3e102976e85d
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-02-16 09:09:10 +0530

    Day09

--- src/day02/Program01.java
+++ src/day02/Program01.java
@@ -1,10 +1,33 @@
-package day02;
+package day02; // pakage name must starts with LC, can have UC after the firstletter, Ex: day02A
 
-public class Program01 {
+public class Program01 { // class name starts with UC
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Good");
+		System.out.println("Day");
+		fun();
 	}
-
+	public static void fun() //functions or methods
+	{
+		int x=10;
+		char a='A';
+		boolean b=true;
+		byte a1=127;
+		short s1=32000;
+		long l1=1234567890l;
+		double a2;
+		a2=12345.4567d;
+		float a3;
+		a3=23456.67f;
+		System.out.println("Have a Great Day!!!");
+		System.out.println(x*x);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a1);
+		System.out.println(s1);
+		System.out.println(l1);
+		System.out.println(a2);
+		System.out.println(a3);
+	}	
 }
