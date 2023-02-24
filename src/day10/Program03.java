96ea2cbcd2a5aab03e65c3be4c74a7badb533e86
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-02-24 08:16:36 +0530

    Day10

---
+++ src/day10/Program03.java
@@ -0,0 +1,28 @@
+package day10;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int table=2;
+		int a=1;
+		while (a<=10)
+		{
+			System.out.println(table+"x"+a+"="+table*a);
+			a=a+1;
+		}
+		System.out.println("******");
+		int b=1;
+		do
+		{
+			System.out.println(table+"x"+b+"="+table*b);
+			b=b+1;
+		}while (b<=10);
+		System.out.println("******");
+		for(int c=1; c<=10; c=c+1)
+		{
+			System.out.println(table+"x"+c+"="+table*c);
+		}
+	}
+
+}
