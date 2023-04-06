ca862458cf005013afe904a3aeb5097288e5f9e8
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-04-06 21:04:08 +0530

    day03.test01

--- src/day03/test01/Program02.java
+++ src/day03/test01/Program02.java
@@ -3,10 +3,10 @@ package day03.test01;
 public class Program02 extends Program01{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Inheritance using extends Keyword
 		Program02 obj1= new Program02(); 
-		obj1.addMethod(10, 20);
-		subMethod(20, 10);
+		obj1.addMethod(10, 20);     //non-Static method
+		subMethod(20, 10);			//static method
 		mulMethod(20, 30);
 	}
 
