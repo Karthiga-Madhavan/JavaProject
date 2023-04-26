f4b84c42c3821e66bbd17e72f7e68aaffd761356
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-04-26 22:13:28 +0530

    HashSet, HashMap, HashTable

--- src/day16/Program02.java
+++ src/day16/Program02.java
@@ -5,16 +5,17 @@ import java.util.Map.Entry;
 
 public class Program02 {
 	
-	//HashMap with Key and Values. Values can be same but keys cannot.
-	//HashMap saves Values in Ascending order (Sequence order)
+	//HashMap with Key and Values. Values can be same but keys cannot (only unique).
+	//HashMap saves Values in Sequence order
 	//HashMap allows null key and null value
 	
 	public static void main(String[] args) {
 		HashMap<String,Integer> x= new HashMap<>();
-		x.put("aa", 101);
+		x.put("aa", 101); //it is the combined form of set(keys) and list(values)
 		x.put("bb", 102);
-		x.put("cc", 103);
-		x.put("dd", 104);
+		x.put("dd", 103);
+		x.put("cc", 104);
+		x.put("aa", 108); // Duplication of key is allowed but it will replace the value of old instance with new value.
 		x.put("ee", 105);
 		x.put(null, 105);
 		x.put("ff", null);
